05c8079a08d160e78e150f53872f117fec4e828a
Pedro Carrillo <pf.cr@icloud.com>
2024-02-22 07:35:37 -0500

    Initial Commit

---
+++ Meli/MeliApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeliApp.swift
+//  Meli
+//
+//  Created by Pedro Carrillo on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MeliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
